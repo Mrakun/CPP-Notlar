2e527188ae27eaacd49fa0bc830334f80883eae3
Burak <buluata35@gmail.com>
2025-09-04 05:11:50 +0300

    9_2

---
+++ Ders9 - String Yapısı/ders9_3.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+using namespace std;
+
+bool HarfKontrol(string kelime) {
+    for (int i = 0; i < kelime.length(); i++) {
+    if (!isalpha(kelime[i]) && kelime[i] != ' ') {
+        return false;
+    }
+}
+return true;
+}
+
+
+int main() {
+
+int n;
+vector<string> isimler(n);
+
+    while (true) {
+        cout << "Kac tane isim gireceksiniz?\nCevap: ";
+        cin >> n;
+        if (cin.fail()) {
+            cout << "\nGecersiz Deger, lutfen bir sayi giriniz.\n" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        break;
+    }
+    
+    for (int i = 0; i < n; i++) {
+
+        while (true) {
+        cout << "\n" << i+1 << ". kişinin tam ismini giriniz: ";    
+        getline(cin, isimler[i]); 
+            
+            if (!HarfKontrol(isimler[i])) {
+                cout << "Lutfen isim icerisinde sayi girmeyin." << endl;
+                continue;
+            }
+
+        break;
+        }
+    }
+
+int enuzunkelime = 0;
+    for (int i = 0; i < n; i++) {
+        if (isimler[i].length() > isimler[enuzunkelime].length()) {
+            enuzunkelime = i;
+        }
+    }
+    cout << "En uzun isim: " << isimler[enuzunkelime] << endl;
+
+return 0;
+}
